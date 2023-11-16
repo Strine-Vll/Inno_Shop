f13a49c762e3930ef88e90628c17dcd817dcf3f1
OShelest73 <90648269+OShelest73@users.noreply.github.com>
2023-11-16 09:10:35 +0300

    Delete AuthenticationManager directory

--- AuthenticationManager/Models/AuthenticationRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AuthenticationManager.Models
-{
-    public class AuthenticationRequest
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
--- AuthenticationManager/Models/AuthenticationResponce.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AuthenticationManager.Models
-{
-    public class AuthenticationResponce
-    {
-        public string UserName { get; set; }
-        public string JwtToken { get; set; }
-        public int ExpiresIn { get; set; }
-    }
-}
